d92abb174ea44c2607458845ac1dcd99eb340927
SEONGHUN-YU <121845879+SEONGHUN-YU@users.noreply.github.com>
2023-04-04 17:35:46 +0900

    Add files via upload

--- March/Mar28_1_HTTP/p12_convertBlog.py
+++ March/Mar28_1_HTTP/p12_convertBlog.py
@@ -3,7 +3,7 @@ from pymongo.mongo_client import MongoClient
 
 con = MongoClient("192.168.0.152")
 db = con.xe
-f = open("D:/yu/daumBlog.csv", "a", encoding="utf-8")
+f = open("D:/yu/daumBlog.txt", "a", encoding="utf-8")
 for d in db.daumBlog.find():
     f.write('%s\t%s\t%s\n' % (d['title'], d['contents'], d['blogname']))
 f.close()
