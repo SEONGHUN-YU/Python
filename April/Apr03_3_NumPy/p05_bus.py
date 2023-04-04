69239e383c7918295d39d027ec01a8096706384b
SEONGHUN-YU <121845879+SEONGHUN-YU@users.noreply.github.com>
2023-04-04 17:38:29 +0900

    Add files via upload

--- April/Apr03_3_NumPy/p05_bus.py
+++ April/Apr03_3_NumPy/p05_bus.py
@@ -9,19 +9,33 @@ f = open("D:/yu/BusResult.txt", "r", encoding="utf-8")
 #        다 읽어서 RAM에다 넣겠다는 건데
 #        전처리를 해오니 가능한 것
 
-bus_stop_on, bus_stop_off, on_list, off_list = [], [], [], []
-for line in f.readlines():
-    s = line.replace("\n", "").split(" ")
-#     if "_타" in s:
-#         bus_stop_on.append(s[0])
-#         on_list.append(s[1])
+# 쌤의 풀이
+# stationName, ride, alight = [], [], []
+# even = True
+# for line in f.readlines():
+#     line = line.replace("\n", "").split("\t")
+#     if even:
+#         stationName.append(line[0][0:len(line[0]) - 2]) # !!
+#         alight.append(int(line[1]))
 #     else:
-#         bus_stop_off.append(s[0])
-#         off_list.append(s[1])
-# bs_on = np.array(bus_stop_on)
-# bs_off = np.array(bus_stop_off)
-# on = np.array(on_list)
-# off = np.array(off_list)
-# print(bs_on[on < off])
-# print(bs_off[on < off])
+#         ride.append(int(line[1]))
+#     even = not even
+
+bus_stop_list, on_list, off_list = [], [], []
+for line in f.readlines():
+    s = line.replace("\n", "").split("\t")
+    if "_내" in line:
+        bus_stop_list.append(s[0].replace("_내", ""))
+        on_list.append(int(s[1]))
+    else:
+        off_list.append(int(s[1]))
+    
+bus_stop = np.array(bus_stop_list)
+on = np.array(on_list)
+off = np.array(off_list)
+print(bus_stop[on < off])
+busResult = bus_stop[on < off]
+for r in busResult:
+    print(r)
+
 f.close()
